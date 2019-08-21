b7204447f45c1f834eaded6838d7e5c9f13876a0
Wang Xing <wangstar1031@hotmail.com>
2019-08-21 21:18:03 +0800

    Inserted removing duplication logic.

--- index.php
+++ index.php
@@ -145,6 +145,22 @@ for( $i = $pageFrom; $i <= $pageTo; $i++){
 	}
 
 }
+// $srcFileName = "31_0_1_157.csv";
+$fileContents = file_get_contents($fileName);
+$arrContents = explode(PHP_EOL, $fileContents);
+$arrRealContents = [];
+$arrPhonenumbers = [];
+foreach ($arrContents as $value) {
+	if( $value == "")
+		continue;
+	$arrBuff = explode(",", $value);
+	$phoneNumber = $arrBuff[count($arrBuff) - 1];
+	if( !in_array( $phoneNumber, $arrPhonenumbers)){
+		$arrPhonenumbers[] = $phoneNumber;
+		$arrRealContents[] = $value;
+	}
+}
+file_put_contents($srcFileName, implode(PHP_EOL, $arrRealContents));
 // Sample url : http://localhost/phone_check/?categoryId=630&from=1&to=167
 ?>
 
